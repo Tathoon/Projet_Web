2b88759d6f772fbc6d3cc183f83bb1c21efeb3c7
Nathan <nathan69004@gmail.com>
2024-04-12 12:54:29 +0200

    navbar v1

--- pages/admin/dashboard_admin.php
+++ pages/admin/dashboard_admin.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - Admin</title>
     <link rel="stylesheet" href="../../styles.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+
 </head>
 <body>
 <?php
@@ -23,6 +25,57 @@
         exit();
     }
 ?>
+
+<header class="page-header">
+  <nav>
+    <img src="../../images/Logo-Web.png" alt="" style="height: 200px;" class="logo">
+    <ul class="admin-menu">
+      <li class="menu-heading">
+        <h3>Admin</h3>
+      </li>
+      <li>
+        <a href="#0">
+          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf201;</i>Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="#0">
+          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf0c0;</i>Utilisateurs</span>
+        </a>
+      </li>
+      <li>
+        <a href="#0">
+          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf145;</i>Tickets</span>
+        </a>
+      </li>
+      <li class="menu-heading">
+        <h3>Settings</h3>
+      </li>
+      <li>
+        <a href="#0">
+          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf013;</i>Paramètre</span>
+        </a>
+      </li>
+      <li>
+        <a href="#0">
+          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf0f3;</i>Notifications</span>
+        </a>
+      </li>
+      <li>
+        <a href="#0">
+          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf08b;</i>Déconnexion</span>
+        </a>
+      </li>
+      <li>
+        <div class="switch">
+          <label for="mode">
+          </label>
+        </div>
+      </li>
+    </ul>
+  </nav>
+</header>
+
 <form method="post">
     <button type="submit" name="logout" class="btn btn-warning" style="display: block; margin: auto;">Déconnecter</button>
 </form>

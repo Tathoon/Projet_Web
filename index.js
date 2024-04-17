f78873b871e6b79890efacd9f2ad502b2277c210
Nathan <nathan69004@gmail.com>
2024-04-17 14:53:00 +0200

    navbar v2

---
+++ index.js
@@ -0,0 +1,12 @@
+/* ------------------*/
+/*                   */
+/*                   */
+/*      NAVBAR       */
+/*                   */
+/*                   */
+/*-------------------*/
+$(document).ready(function(){
+    $('.nav_btn').click(function(){
+      $('.mobile_nav_items').toggleClass('active');
+    });
+  });
--- pages/admin/dashboard_admin.php
+++ pages/admin/dashboard_admin.php
@@ -1,85 +1,75 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Admin</title>
-    <link rel="stylesheet" href="../../styles.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard - Admin</title>
+  <link rel="stylesheet" href="../../styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
 </head>
 <body>
-<?php
+  <?php
     session_start();
-
     echo $_SESSION['nom'];
 
     if (!isset($_SESSION['role']) || ($_SESSION['role'] != 1 )) {
-        header('Location: index.php');
-        exit();
+      header('Location: index.php');
+      exit();
     }
 
     if(isset($_POST['logout'])) {
-        session_destroy();
-        header('Location: ../../index.php');
-        exit();
+      session_destroy();
+      header('Location: ../../index.php');
+      exit();
     }
-?>
+  ?>
 
-<header class="page-header">
-  <nav>
-    <img src="../../images/Logo-Web.png" alt="" style="height: 200px;" class="logo">
-    <ul class="admin-menu">
-      <li class="menu-heading">
-        <h3>Admin</h3>
-      </li>
-      <li>
-        <a href="#0">
-          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf201;</i>Dashboard</span>
-        </a>
-      </li>
-      <li>
-        <a href="#0">
-          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf0c0;</i>Utilisateurs</span>
-        </a>
-      </li>
-      <li>
-        <a href="#0">
-          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf145;</i>Tickets</span>
-        </a>
-      </li>
-      <li class="menu-heading">
-        <h3>Settings</h3>
-      </li>
-      <li>
-        <a href="#0">
-          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf013;</i>Paramètre</span>
-        </a>
-      </li>
-      <li>
-        <a href="#0">
-          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf0f3;</i>Notifications</span>
-        </a>
-      </li>
-      <li>
-        <a href="#0">
-          <span><i class="fas" style="font-family:Arial, FontAwesome">&#xf08b;</i>Déconnexion</span>
-        </a>
-      </li>
-      <li>
-        <div class="switch">
-          <label for="mode">
-          </label>
-        </div>
-      </li>
-    </ul>
-  </nav>
-</header>
+  <input type="checkbox" id="check">
+  <header>
+    <label for="check">
+      <i class="fas fa-bars" id="sidebar_btn"></i>
+    </label>
+    <div class="left_area">
+      <h3>E11<span>event</span></h3>
+    </div>
+    <div class="right_area">
+      <form method="post" action="../../index.php">
+        <button type="submit" name="logout" class="logout_btn">Logout</button>
+      </form>
+    </div>
+  </header>
 
-<form method="post">
-    <button type="submit" name="logout" class="btn btn-warning" style="display: block; margin: auto;">Déconnecter</button>
-</form>
-    <h1 class="red">ADMIN</h1>
-    <script type="text/javascript" src="../../index.js"></script>
+  <div class="mobile_nav">
+    <div class="nav_bar">
+      <img src="../../images/Logo-Web.png" class="mobile_profile_image" alt="">
+      <i class="fa fa-bars nav_btn"></i>
+    </div>
+    <div class="mobile_nav_items">
+      <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
+      <a href="#"><i class="fas fa-cogs"></i><span>Tickets</span></a>
+      <a href="#"><i class="fas fa-table"></i><span>Utilisateurs</span></a>
+      <a href="#"><i class="fas fa-th"></i><span>Forms</span></a>
+      <a href="#"><i class="fas fa-info-circle"></i><span>About</span></a>
+      <a href="#"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
+    </div>
+  </div>
+
+  <div class="sidebar">
+    <div class="profile_info">
+      <img src="1.png" class="profile_image" alt="">
+      <h4>Admin</h4>
+    </div>
+    <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
+    <a href="#"><i class="fas fa-cogs"></i><span>Components</span></a>
+    <a href="#"><i class="fas fa-table"></i><span>Tables</span></a>
+    <a href="#"><i class="fas fa-th"></i><span>Forms</span></a>
+    <a href="#"><i class="fas fa-info-circle"></i><span>About</span></a>
+    <a href="#"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
+  </div>
+
+  <h1 class="red">ADMIN</h1>
+  
+  <script type="text/javascript" src="../../index.js"></script>
 </body>
 </html>

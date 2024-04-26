dedb7193780142457d1c48f15854c2d9a12cee9f
Nathan <nathan69004@gmail.com>
2024-04-26 10:01:49 +0200

    logo onglet

--- pages/autres/settings.php
+++ pages/autres/settings.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Paramètres</title>
+  <link rel="icon" href="../../images/Logo_onglet.png" type="image/x-icon">
   <link rel="stylesheet" href="../../styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>

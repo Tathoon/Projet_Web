164d3a07b97d9bb9f1cc91709339d3c382effec8
Nathan <nathan69004@gmail.com>
2024-04-26 16:05:24 +0200

    setting page v2

--- pages/autres/settings.php
+++ pages/autres/settings.php
@@ -83,98 +83,82 @@
     }
     </script>
   </div>
-  <div class="content">
-  <main>
-    <div class="avatar_section">
-      <div class="header">
-        <h1>Changez votre avatar</h1>
-      </div>
-      <div class="avatar_options" id="avatarContainer">
-        <label for="avatar1">
-          <img src="../../images/avatar/avatar1.png" alt="Avatar 1">
-          <input type="radio" id="avatar1" name="avatar" value="avatar1.jpg">
-        </label>
-        <label for="avatar2">
-          <img src="../../images/avatar/avatar2.png" alt="Avatar 2">
-          <input type="radio" id="avatar2" name="avatar" value="avatar2.jpg">
-        </label>
-        <label for="avatar3">
-          <img src="../../images/avatar/avatar3.png" alt="Avatar 3">
-          <input type="radio" id="avatar3" name="avatar" value="avatar3.jpg">
-        </label>
-        <label for="avatar4">
-          <img src="../../images/avatar/avatar4.png" alt="Avatar 4">
-          <input type="radio" id="avatar4" name="avatar" value="avatar4.jpg">
-        </label>
-        <label for="avatar5">
-          <img src="../../images/avatar/avatar5.png" alt="Avatar 5">
-          <input type="radio" id="avatar5" name="avatar" value="avatar5.jpg">
-        </label>
+    <div class="content">
+    <main>
+      <div class="avatar_section">
+        <div class="header">
+          <h1>Changez votre avatar</h1>
+        </div>
+        <div class="avatar_options" id="avatarContainer">
+          <label for="avatar1">
+            <img src="../../images/avatar/avatar1.png" alt="Avatar 1">
+            <input type="radio" id="avatar1" name="avatar" value="avatar1.jpg">
+          </label>
+          <label for="avatar2">
+            <img src="../../images/avatar/avatar2.png" alt="Avatar 2">
+            <input type="radio" id="avatar2" name="avatar" value="avatar2.jpg">
+          </label>
+          <label for="avatar3">
+            <img src="../../images/avatar/avatar3.png" alt="Avatar 3">
+            <input type="radio" id="avatar3" name="avatar" value="avatar3.jpg">
+          </label>
+          <label for="avatar4">
+            <img src="../../images/avatar/avatar4.png" alt="Avatar 4">
+            <input type="radio" id="avatar4" name="avatar" value="avatar4.jpg">
+          </label>
+          <label for="avatar5">
+            <img src="../../images/avatar/avatar5.png" alt="Avatar 5">
+            <input type="radio" id="avatar5" name="avatar" value="avatar5.jpg">
+          </label>
 
-        <button id="showMoreAvatars">Surprise !</button>
+          <button id="showMoreAvatars">Surprise !</button>
 
-        <div id="hiddenAvatars" class="avatar_options" style="display: none;">
-          <label for="avatar6">
-            <img src="../../images/avatar/dancing-toothless-tothless.gif" alt="Avatar 6">
-            <input type="radio" id="avatar6" name="avatar" value="avatar6.jpg">
-          </label>
-          <label for="avatar7">
-            <img src="../../images/avatar/Donald-Duck.gif" alt="Avatar 7">
-            <input type="radio" id="avatar7" name="avatar" value="avatar7.jpg">
-          </label>
-          <label for="avatar8">
-            <img src="../../images/avatar/Pedro.gif" alt="Avatar 8">
-            <input type="radio" id="avatar8" name="avatar" value="avatar8.jpg">
-          </label>
-          <label for="avatar9">
-            <img src="../../images/avatar/PowerRanger.gif" alt="Avatar 9">
-            <input type="radio" id="avatar9" name="avatar" value="avatar9.jpg">
-          </label>
-          <label for="avatar10">
-            <img src="../../images/avatar/angry-cat.gif" alt="Avatar 10">
-            <input type="radio" id="avatar10" name="avatar" value="avatar10.jpg">
-          </label>
-          <label for="avatar11">
-            <img src="../../images/avatar/quokka.gif" alt="Avatar 11">
-            <input type="radio" id="avatar11" name="avatar" value="avatar11.jpg">
-          </label>
+          <div id="hiddenAvatars" class="avatar_options" style="display: none;">
+            <label for="avatar6">
+              <img src="../../images/avatar/dancing-toothless-tothless.gif" alt="Avatar 6">
+              <input type="radio" id="avatar6" name="avatar" value="avatar6.jpg">
+            </label>
+            <label for="avatar7">
+              <img src="../../images/avatar/Donald-Duck.gif" alt="Avatar 7">
+              <input type="radio" id="avatar7" name="avatar" value="avatar7.jpg">
+            </label>
+            <label for="avatar8">
+              <img src="../../images/avatar/Pedro.gif" alt="Avatar 8">
+              <input type="radio" id="avatar8" name="avatar" value="avatar8.jpg">
+            </label>
+            <label for="avatar9">
+              <img src="../../images/avatar/PowerRanger.gif" alt="Avatar 9">
+              <input type="radio" id="avatar9" name="avatar" value="avatar9.jpg">
+            </label>
+            <label for="avatar10">
+              <img src="../../images/avatar/angry-cat.gif" alt="Avatar 10">
+              <input type="radio" id="avatar10" name="avatar" value="avatar10.jpg">
+            </label>
+            <label for="avatar11">
+              <img src="../../images/avatar/quokka.gif" alt="Avatar 11">
+              <input type="radio" id="avatar11" name="avatar" value="avatar11.jpg">
+            </label>
+          </div>
         </div>
       </div>
-    </div>
-  </main>
-</div>
+    </main>
+  </div>
 
 
-    <script>
-      // Sélectionnez les boutons radio et les images
-      var radios = document.querySelectorAll('input[type=radio][name="avatar"]');
-      var mobileProfileImage = document.querySelector('.mobile_profile_image');
-      var profileImage = document.querySelector('.profile_image');
+  <script>
 
-      // Récupérez l'avatar sélectionné du stockage local, s'il existe
-      var selectedAvatar = localStorage.getItem('selectedAvatar');
-      if (selectedAvatar) {
-          mobileProfileImage.src = selectedAvatar;
-          profileImage.src = selectedAvatar;
-      }
+    // Sélectionnez les boutons radio et les images
+    var radios = document.querySelectorAll('input[type=radio][name="avatar"]');
+    var mobileProfileImage = document.querySelector('.mobile_profile_image');
+    var profileImage = document.querySelector('.profile_image');
 
-      // Ajoutez un écouteur d'événements à chaque bouton radio
-      radios.forEach(function(radio) {
-          radio.addEventListener('change', function() {
-              // Modifiez les attributs src des images lorsque le bouton radio est sélectionné
-              var avatarSrc;
-              if (this.id === 'avatar1') {
-                  avatarSrc = '../../images/Logo-Web.png';
-              } else if (this.id === 'avatar2') {
-                  avatarSrc = '../../images/Logo_onglet.png';
-              }
-              mobileProfileImage.src = avatarSrc;
-              profileImage.src = avatarSrc;
+    // Récupérez l'avatar sélectionné du stockage local, s'il existe
+    var selectedAvatar = localStorage.getItem('selectedAvatar');
+    if (selectedAvatar) {
+        mobileProfileImage.src = selectedAvatar;
+        profileImage.src = selectedAvatar;
+    }
 
-              // Enregistrez l'avatar sélectionné dans le stockage local
-              localStorage.setItem('selectedAvatar', avatarSrc);
-          });
-      });
   </script>
   <script type="text/javascript" src="../../index.js"></script>
 </body>
